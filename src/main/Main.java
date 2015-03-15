d62965d1fd64aaf5543123740de8c4a206f1d401
Zopper9000 <david.lu@hotmail.com>
2015-03-15 00:28:42 -0400

    Added basic grid elements

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 import battle.Battle;
 import grid.Point;
 import character.Monster;
@@ -8,48 +10,62 @@ import character.Player;
 import character.Role;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
 		
 		//Name, Strength, Perception, Endurance, Charisma, Intelligence, Agility, Luck, Location
 		Player a = new Player("Derp", new Point(0,0), Role.Warrior);
 		
-		MonsterList monster = new MonsterList();
-		Monster m;
-		Battle b;
-		while(a.getLevel() < a.maxLevel)
+		int choice;
+		System.out.println("Choose your adventure!");
+		System.out.println("1. Battle");
+		System.out.println("2. Movement");
+		choice = in.nextInt();
+		switch(choice)
 		{
-			m = monster.nextMonster(a.getLevel());
-			b = new Battle(a, m);
-			
-			while(b.isVisible())
+		case 1:
+			MonsterList monster = new MonsterList();
+			Monster m;
+			Battle b;
+			while(a.getLevel() < a.maxLevel)
 			{
-				try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				m = monster.nextMonster(a.getLevel());
+				b = new Battle(a, m);
+				
+				while(b.isVisible())
+				{
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				if(a.getHp() > 0)
+				{
+					a.gainExp(m.getExpGained());
+					System.out.println("You've gained " + m.getExpGained() + " experience points!");
+					System.out.println("You are currently level " + a.getLevel() + ".");
+				}
+				else
+				{
+					System.out.println("You are dead.");
+					break;
 				}
 			}
 			
-			if(a.getHp() > 0)
+			if(a.getLevel() > a.maxLevel)
 			{
-				a.gainExp(m.getExpGained());
-				System.out.println("You've gained " + m.getExpGained() + " experience points!");
-				System.out.println("You are currently level " + a.getLevel() + ".");
+				System.out.println("You have won the game!");
 			}
-			else
-			{
-				System.out.println("You are dead.");
-				break;
-			}
+			break;
+		case 2:
+			System.out.println("Incomplete");
+			break;
 		}
-		
-		if(a.getLevel() > a.maxLevel)
-		{
-			System.out.println("You have won the game!");
-		}
-
 	}
 
 }
